981f51230576ad6c302835be0b76d5448e38a434
Yousuf Zia <Yousuf Zia@10.0.0.180>
2021-09-30 21:12:08 -0400

    changes to xxx user story

--- src/demo/DemoOne.java
+++ src/demo/DemoOne.java
@@ -4,6 +4,14 @@ public class DemoOne {
 	public static void main(String[] args) {
 		
 		System.out.println("Git Demo");
+		
+		boolean isSunday = true;
+		
+		if(isSunday) {
+			System.out.println("abc");
+		}else {
+			System.out.println("xyz");
+		}
 	}
 
 }
